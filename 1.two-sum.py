2a39426c4fc9930da50b1c976a3cac51acd9faa8
X0buf <expbuf@gmail.com>
2021-03-02 11:35:17 +0800

    leetcode learn start

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -16,7 +16,7 @@ class Solution:
         lens = len(nums)
         j = -1
         for i in range(lens):
-            if (target - nums[i]) in nums:X-buf
+            if (target - nums[i]) in nums:
                 if (nums.count(target - nums[i]) == 1) & (
                         target - nums[i] == nums[i]):  # 如果num2=num1,且nums中只出现了一次，说明找到是num1本身。
                     continue
