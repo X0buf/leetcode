3763da5e9a18971a8d58c025bf92f00053b17ba2
X0buf <expbuf@gmail.com>
2021-03-23 19:49:40 +0800

    new readme

---
+++ 31.next-permutation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+   @FileName :   31.next-permutation.py
+   @Email :      expbuf@hotmail.com
+   @Date :       2021/03/23 19:46
+   @Author :     expbuf
+   @Description :
+"""
+__author__ = 'expbuf'
+
+# if __name__ == '__main__':
