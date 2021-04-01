4ccb38dcebbaffb35182b102c14bcaefbccb5573
X0buf <expbuf@gmail.com>
2021-04-01 18:10:24 +0800

    new solution

---
+++ 32.longest-valid-parentheses.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+   @FileName :   32.longest-valid-parentheses.py
+   @Email :      expbuf@hotmail.com
+   @Date :       2021/04/01 18:10
+   @Author :     expbuf
+   @Description :
+"""
+__author__ = 'expbuf'
+
+# if __name__ == '__main__':
